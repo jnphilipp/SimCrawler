8f4863e1b9d1d8858f91dd5fa22ceb90c4330cf6
proewer <paul.roewer@freenet.de>
2014-11-04 15:54:02 +0100

    Added QualityMappingReader and WebGraphReader.

--- src/main/java/org/simcrawler/App.java
+++ src/main/java/org/simcrawler/App.java
@@ -1,13 +1,48 @@
 package org.simcrawler;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.simcrawler.io.files.QualityMappingReader;
+import org.simcrawler.io.files.WebGraphReader;
+
 /**
  *
- * @author jnphilipp
+ * @author jnphilipp, proewer
  * @version 0.0.1
  */
 public class App {
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws IOException {
 		Logger.getInstance();
 		Logger.info("SimCrawler");
+		int k;
+		Map qualityMap, webGraph;
+		File seedFile, stepQuality;
+		if ( args.length != 0 ) {
+			List<String> l = Arrays.asList(args);
+			Iterator<String> it = l.iterator();
+			while ( it.hasNext() ) {
+				switch ( it.next() ) {
+					case "--k":k = Integer.parseInt(it.next());
+					case "--qm":qualityMap=QualityMappingReader.read(it.next());break;
+					case "--wg":webGraph=WebGraphReader.read(it.next());break;
+					case "--sf":seedFile = new File(it.next());break;
+					case "--sq":stepQuality = new File(it.next());break;
+				}
+			}
+		}
+		else{
+			Logger.info("input parameters:"
+					+ "\n\t--k : urls per crawling step"
+					+ "\n\t--qm : quality mapping input file"
+					+ "\n\t--wg : web graph input file"
+					+ "\n\t-- sf: seed input file"
+					+ "\n\t--sq : step quality output file");
+		}
 	}
 }
---
+++ src/main/java/org/simcrawler/io/files/WebGraphReader.java
@@ -0,0 +1,33 @@
+package org.simcrawler.io.files;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.simcrawler.io.FileReader;
+
+/**
+ * 
+ * @author paul
+ * @version 0.0.1
+ */
+public class WebGraphReader {
+
+	public static Map<String,Set<String>> read(String file) throws IOException{
+		Map<String,Set<String>> webGraph = new LinkedHashMap<>();
+		List<String[]> result = new ArrayList<>();
+		FileReader.readCSV(file, result, "\t");
+		for(int i=0;i<result.size();i++){
+			if(!webGraph.containsKey(result.get(i)[0])){
+				webGraph.put(result.get(i)[0], new LinkedHashSet<String>());
+			}
+			webGraph.get(result.get(i)[0]).add(result.get(i)[1]);
+		}
+				
+		return webGraph;
+	}
+}
